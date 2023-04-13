262a9871b31f6d06834f25c3d8653ed26fbc2479
Dorota Wlazlo <dori.wlazlo@gmail.com>
2023-04-13 19:42:20 +0200

    all plots

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ def get_attribute(data_list, attribute_number):
     result = []
     for i in data_list:
         line = i.split()
-        result.append(line[attribute_number])
+        result.append(float(line[attribute_number]))
     return result
 
 
 def draw_histogram(data, name, suffix):
-    plt.hist(data, 10)
+    plt.clf()
+    plt.hist(data)
     plt.xlabel("Value (mm)")
     plt.ylabel("Frequency")
     result = suffix.replace(' ', '_')
     plt.title(name)
     result = 'plots/' + result + '.png'
     plt.savefig(result)
-    plt.clf()
     plt.close()
 
 
@@ -31,21 +31,21 @@ def generate(data_list, name):
         short_name = name + suffix[j]
         attribute_values = get_attribute(data_list, i)
         draw_histogram(attribute_values, long_name, short_name)
-        long_name = ""
-        short_name = ""
 
 
 
-
-def print_hi(name):
+def main():
     with open('crabs.dat', 'r') as f:
         message = f.read()
     f.close()
     lines = message.splitlines()
     generate(lines[1:51], "Blue male")
+    generate(lines[51:101], "Blue female")
+    generate(lines[101:151], "Orange male")
+    generate(lines[151:201], "Orange female")
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 
