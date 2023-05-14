e612171c5b7309492f4438e91a9d74da1312aaaa
Dorota Wlazlo <dori.wlazlo@gmail.com>
2023-05-14 17:53:35 +0200

    data preparation fixes

--- data_preparation.py
+++ data_preparation.py
@@ -9,14 +9,29 @@ def load_file(file_name):
     return lines
 
 
+def class_convertion(lines):
+    i = 0
+    for line in lines:
+        if line[0] == 'B' and line[2] == 'M':
+            lines[i] = '1 0 0 0 ' + line
+        if line[0] == 'B' and line[2] == 'F':
+            lines[i] = '0 1 0 0 ' + line
+        if line[0] == 'O' and line[2] == 'M':
+            lines[i] = '0 0 1 0 ' + line
+        if line[0] == 'O' and line[2] == 'F':
+            lines[i] = '0 0 0 1 ' + line
+        i += 1
+    return lines
+
+
 def data_scaling(lines):
     lines = lines[1:]
 
     data = [line.split() for line in lines]
     array = np.array(data)
-    numeric_columns = [1, 2, 3, 4, 5]
-    columns_to_remove = [0, 1]
+    columns_to_remove = [4, 5, 6]
     array = np.delete(array, columns_to_remove, axis=1)
+    numeric_columns = [4, 5, 6, 7, 8]
     array = array.astype(np.float64)
 
     for col in numeric_columns:
@@ -24,7 +39,7 @@ def data_scaling(lines):
         max_val = np.max(array[:, col])
         array[:, col] = (array[:, col] - min_val) / (max_val - min_val) * 2 - 1
 
-    print(array)
+    return array
 
 
 def float_converter(value):
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from data_preparation import load_file, data_scaling
+from data_preparation import load_file, class_convertion, data_scaling
 from histogram_generation import generate_all_histograms
 
 
 def main():
     lines = load_file('crabs.dat')
-    #generate_all_histograms(lines)
-    data_scaling(lines)
+    lines_converted = class_convertion(lines)
+    data_array = data_scaling(lines_converted)
+    print(data_array[100])
 
 
 if __name__ == '__main__':
